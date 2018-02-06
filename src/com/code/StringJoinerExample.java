eddb8a27a4c1ec2fbef260a8b50bdd7715d20af9
Rahul Soni <rahul.soni16@gmail.com>
2018-02-06 20:54:31 +0530

    StringJoiner

---
+++ src/com/code/StringJoinerExample.java
@@ -0,0 +1,20 @@
+package com.code;
+
+import java.util.StringJoiner;
+
+public class StringJoinerExample {
+
+    public static void main(String[] args) {
+
+	String suffix = null;
+	String fName = null;
+	String lName = "Soni";
+	StringJoiner sj = new StringJoiner(",");
+	sj.add("Rahul").add("Soni").add(suffix == null ? "" : suffix);
+	System.out.println(sj.toString());
+
+	System.out.println(fName + " " + lName);
+
+    }
+
+}
